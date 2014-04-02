01f8395838931c824833f3af1ccd1ad4cbfe38eb
Nucleareal <nuclearfusionnuclearfusion@yahoo.co.jp>
2014-04-02 20:25:37 +0900

    Initial commit

---
+++ RandomPiecesGenerator/Launcher.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+namespace RandomPiecesGenerator
+{
+    class Launcher
+    {
+        private static Cn Cns
+        {
+            set;
+            get;
+        }
+
+        public static void Main(string[] args)
+        {
+            Cns = new Cn();
+
+            bool isAvailableArgs = args.Length > 0;
+
+            if (!isAvailableArgs)
+            {
+                PutUsage();
+                return;
+            }
+
+            var opts = new List<string>();
+            var names = new List<string>();
+
+            bool isOptEnd = false;
+
+            foreach (var v in args)
+            {
+                isOptEnd |= v.StartsWith("--");
+
+                if (v.StartsWith("-") && !isOptEnd)
+                {
+                    opts.Add(v);
+                }
+                else
+                {
+                    names.Add(v);
+                }
+            }
+
+            bool isShuffle = false;
+
+            if (isAvailableArgs)
+            {
+                foreach (var v in opts)
+                {
+                    if (v.Contains("-q"))
+                    {
+                        Cns = new NCn();
+                    }
+                    if (v.Contains("-h"))
+                    {
+                        PutUsage();
+                        return;
+                    }
+                    if (v.Contains("-r"))
+                    {
+                        isShuffle |= true;
+                    }
+                }
+            }
+
+            foreach(var v in names)
+            {
+                if (!File.Exists(v))
+                {
+                    Cns.Error("ImageFile {0} Is Not Exist", v);
+                    continue;
+                }
+
+                using (Bitmap img = new Bitmap(v))
+                {
+                    var dvx = 16;
+                    var dvy = 16;
+                    var SC = 16;
+                    var CostC = 300;
+                    var CostS = 100;
+
+                    var sps = Path.GetFileName(v).Split('_', '.');
+                    if (sps.Length < 7)
+                    {
+                        Cns.Warning("FileName not Contains Params (You'll read Readme.txt), Use Default Params");
+                        continue;
+                    }
+                    else
+                    {
+                        var cpt = new string[sps.Length - 2];
+
+                        Array.Copy(sps, 1, cpt, 0, sps.Length - 2);
+
+                        var toA = cpt.Select(int.Parse).ToArray();
+                        dvx = toA[0];
+                        dvy = toA[1];
+                        SC = toA[2];
+                        CostC = toA[3];
+                        CostS = toA[4];
+                    }
+
+                    // Filename _ DVX _ DVY _ SetectableCount _ CostRateX _ CostRateY . 拡張子
+
+                    string fname = Regex.Replace(v, @"(png|gif)", "ppm");
+                    int w = img.Width;
+                    int h = img.Height;
+
+                    //Cns.WriteLine("({0}, {1})", w, h);
+
+                    if (w % 16 != 0 || h % 16 != 0)
+                    {
+                        Cns.Warning("Width or Height should be multiples of 16");
+                    }
+
+                    using (StreamWriter sw = new StreamWriter(fname))
+                    {
+                        sw.WriteLine("P6");
+                        sw.WriteLine("# {0} {1}", dvx, dvy);
+                        sw.WriteLine("# {0}", SC);
+                        sw.WriteLine("# {0} {1}", CostC, CostS);
+                        sw.WriteLine("{0} {1}", w, h);
+                        sw.WriteLine("255");
+                    }
+
+                    Shuffler sf = new Shuffler(dvx, dvy, SC, w / dvx, h / dvy);
+                    sf.Init();
+
+                    if (isShuffle)
+                    {
+                        sf.Shuffle();
+                    }
+
+                    int dix = w / dvx;
+                    int diy = h / dvy;
+
+                    using (FileStream fs = new FileStream(fname, FileMode.Append, FileAccess.Write))
+                    {
+                        for (int j = 0; j < h; j++)
+                        {
+                            for (int i = 0; i < w; i++)
+                            {
+                                int inX = j / dix;
+                                int inY = i / diy;
+
+                                int lvX = sf[inX, inY].FromX;
+                                int lvY = sf[inX, inY].FromY;
+
+                                int toX = lvX * dix + i % dix;
+                                int toY = lvY * diy + j % diy;
+
+                                Color c = img.GetPixel(toX, toY);
+
+                                fs.WriteByte(c.R);
+                                fs.WriteByte(c.G);
+                                fs.WriteByte(c.B);
+                            }
+                        }
+                    }
+
+                    //Cns.WriteLine("Yeah");
+                }
+            }
+        }
+
+        private static void PutUsage()
+        {
+            Cns.WriteLine(@"USAGE: .\{0} [Options] [FileNames]", Process.GetCurrentProcess().MainModule.ModuleName);
+            Cns.WriteLine("\t-q: Quiet");
+            Cns.WriteLine("\t-h: Help(this)");
+            Cns.WriteLine("\t-r: Randomize");
+            Cns.WriteLine("\t--: End of Option");
+        }
+
+        private class Cn
+        {
+            public virtual void WriteLine()
+            {
+                Console.WriteLine();
+            }
+
+            public virtual void WriteLine(object o)
+            {
+                Console.WriteLine(o);
+            }
+
+            public virtual void WriteLine(string str, params object[] obj)
+            {
+                Console.WriteLine(str, obj);
+            }
+
+            public virtual void Error(string str, params object[] obj)
+            {
+                WriteLine(string.Format("ERROR: {0}", str), obj);
+            }
+
+            public virtual void Warning(string str, params object[] obj)
+            {
+                WriteLine(string.Format("Warning: {0}", str), obj);
+            }
+        }
+
+        private class NCn : Cn
+        {
+            public override void WriteLine()
+            {
+            }
+
+            public override void WriteLine(string str, params object[] obj)
+            {
+            }
+        }
+    }
+}
---
+++ RandomPiecesGenerator/Shuffler.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RandomPiecesGenerator
+{
+    class Shuffler //画像全体
+    {
+        private Random _rand = new Random();
+
+        public Shuffler(int x, int y, int sc, int xl, int yl)
+        {
+            XCount = x;
+            YCount = y;
+            SelectCount = sc;
+
+            XLength = xl;
+            YLength = yl;
+        }
+
+        private Chunk[,] _chunk;
+
+        public Chunk this[int x, int y]
+        {
+            set
+            {
+                _chunk[y, x] = value;
+            }
+            get
+            {
+                return _chunk[y, x];
+            }
+        }
+
+        public void Init()
+        {
+            _chunk = new Chunk[YCount, XCount];
+            for (int i = 0; i < YCount; i++)
+            {
+                for (int j = 0; j < XCount; j++)
+                {
+                    this[j, i] = new Chunk();
+                    this[j, i].ToX = i;
+                    this[j, i].ToY = j;
+                    this[j, i].FromX = i;
+                    this[j, i].FromY = j;
+                }
+            }
+        }
+
+        private enum Direction
+        {
+            Left  = 14,  // 0b 11 10
+            Up    = 11,  // 0b 10 11
+            Right =  6,  // 0b 01 10
+            Down  =  9,  // 0b 10 01
+        }
+
+        private int XDis(Direction d)
+        {
+            return 2 - ( ((int)d) >> 2 );
+        }
+
+        private int YDis(Direction d)
+        {
+            return 2 - ( ((int)d) & 3 );
+        }
+
+        public void Shuffle()
+        {
+            for (int i = 0; i < SelectCount; i++)
+            {
+                int n = 4 + _rand.Next(XCount * YCount);
+                int fx = _rand.Next(XCount);
+                int fy = _rand.Next(YCount);
+
+                var dirs = new List<Direction>
+                { Direction.Left, Direction.Up, Direction.Right, Direction.Down };
+
+                for (int j = 0; j < n; j++)
+                {
+                    var dirc = dirs[_rand.Next(dirs.Count)];
+                    int dx = XDis(dirc);
+                    int dy = YDis(dirc);
+
+                    if (Clamp(0, LastX, fx) == Clamp(0, LastX, fx + dx) && Clamp(0, LastY, fy) == Clamp(0, LastY, fy + dy))
+                    {
+                        dirs.Remove(dirc);
+                        j--;
+                        continue;
+                    }
+
+                    Swap(fx, fy, fx + dx, fy + dy);
+
+                    fx += dx;
+                    fy += dy;
+
+                    dirs = new List<Direction> { Direction.Left, Direction.Up, Direction.Right, Direction.Down };
+                }
+            }
+        }
+
+        private void Swap(int x1, int y1, int x2, int y2)
+        {
+            Chunk c = this[x1, y1];
+            this[x1, y1] = this[x2, y2];
+            this[x2, y2] = c;
+
+            this[x1, y1].ToX = x2;
+            this[x1, y1].ToY = y2;
+            this[x2, y2].ToX = x1;
+            this[x2, y2].ToY = y1;
+        }
+
+        private int Clamp(int min, int max, int val)
+        {
+            return val < min ? min : max < val ? max : val;
+        }
+
+        private int XCount
+        {
+            set;
+            get;
+        }
+
+        private int YCount
+        {
+            set;
+            get;
+        }
+
+        private int XLength
+        {
+            set;
+            get;
+        }
+
+        private int YLength
+        {
+            set;
+            get;
+        }
+
+        private int SelectCount
+        {
+            set;
+            get;
+        }
+
+        private int LastX
+        {
+            get { return XCount - 1; }
+        }
+
+        private int LastY
+        {
+            get { return YCount - 1; }
+        }
+
+        public void CopyImage(Bitmap from, Bitmap to, int bx, int by)
+        {
+            //Console.WriteLine("Div:{0} {1}, Length:{2} {3}", XCount, YCount, XLength, YLength);
+
+            for (int i = 0; i < XLength; i++)
+            {
+                for (int j = 0; j < YLength; j++)
+                {
+                    int ix = bx * XLength + i;
+                    int iy = by * YLength + j;
+                    int dx = this[bx, by].FromX * XLength + i;
+                    int dy = this[bx, by].FromY * YLength + j;
+                    to.SetPixel(ix, iy, from.GetPixel(dy, dx));
+                }
+            }
+        }
+
+        public class Chunk //断片画像
+        {
+            public int FromX
+            {
+                set;
+                get;
+            }
+
+            public int FromY
+            {
+                set;
+                get;
+            }
+
+            public int ToX
+            {
+                set;
+                get;
+            }
+
+            public int ToY
+            {
+                set;
+                get;
+            }
+        }
+    }
+}
